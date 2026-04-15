d2e05c43a13802b33c38cefa80c015d6ab9d302d
Lucia Alday <lalday@arizona.edu>
2026-04-14 21:54:57 -0700

    track basic format

--- spotify-app/src/components/Basic.jsx
+++ spotify-app/src/components/Basic.jsx
@@ -2,31 +2,25 @@ export default function Basic({ tracks, artists }) {
     return (
         <div className="flex-row space-between" style={{ alignItems: 'flex-start' }}>
             <div style={{ flex: '0 1 auto', maxWidth: '45%' }}>
-                <table>
-                    <tbody>
-                        {tracks && tracks.map((track, idx) => (
-                            <tr key={track.id}>
-                                <td><h3 style={{ width: 30 }}>{idx + 1}</h3></td>
-                                <td><img className='track-art' src={track.album.images[0].url} alt="Album Art" style={{ width: 60, height: 60 }} /></td>
-                                <td>
-                                    <div className='current-track' style={{ margin: '0' }}>
-                                        <h1>{track.name}</h1>
-                                        <h2>{track.artists.map(a => a.name).join(', ')}</h2>
-                                    </div>
-                                </td>
-                            </tr>
-                        ))}
-                        {artists && artists.map((artist, idx) => (
-                            <div key={artist.id} className="flex-row center fade-out text-glow">
-                                <h3 style={{ width: 30 }}> {idx + 1} </h3>
-                                <img className='track-art' src={artist.images[0].url} alt="Artist Image" style={{ width: 60, height: 60 }} />
-                                <div className='current-track'>
-                                    <h1>{artist.name}</h1>
-                                </div>
-                            </div>
-                        ))}
-                    </tbody>
-                </table>
+                {tracks && tracks.map((track, idx) => (
+                    <div key={track.id} className="flex-row center fade-out text-glow">
+                        <h3 style={{ width: 30 }}> {idx + 1} </h3>
+                        <img className='track-art' src={track.album.images[0].url} alt="Album Art" style={{ width: 60, height: 60 }} />
+                        <div className='current-track'>
+                            <h1>{track.name}</h1>
+                            <h2>{track.artists.map(a => a.name).join(', ')}</h2>
+                        </div>
+                    </div>
+                ))}
+                {artists && artists.map((artist, idx) => (
+                    <div key={artist.id} className="flex-row center fade-out text-glow">
+                        <h3 style={{ width: 30 }}> {idx + 1} </h3>
+                        <img className='track-art' src={artist.images[0].url} alt="Artist Image" style={{ width: 60, height: 60 }} />
+                        <div className='current-track'>
+                            <h1>{artist.name}</h1>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     )
